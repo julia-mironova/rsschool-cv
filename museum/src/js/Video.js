abb83eda84cf55e9db2dfd1ae7a3c7f60f40925c
Yuliya Mironova <rabkevich15@gmail.com>
2021-10-10 17:24:11 +0200

    fix:player-cat

--- museum/src/js/Video.js
+++ museum/src/js/Video.js
@@ -1,5 +1,5 @@
 //big player
-let video = document.querySelector(".video");
+let video = document.querySelector(".big-video-individ");
 let bigPlay = document.querySelector(".player-play-big");
 let play = document.querySelector(".player-play-normal");
 let pauseImg = document.querySelector(".toPause");
@@ -14,10 +14,10 @@ let screenImg = document.querySelector(".screenBig");
 function videoStatus() {
   if (video.paused) {
     video.play();
-    pauseImg.src = "img/Museum/pause.png";
+    pauseImg.src = "assets/img/Video/player-btn/pause.png";
   } else {
     video.pause();
-    pauseImg.src = "img/Museum/play.png";
+    pauseImg.src = "assets/img/Video/player-btn/play.png";
   }
 }
 
@@ -32,10 +32,10 @@ function changeRange() {
 
 function soundOnOf() {
   if (progressSound.value != 0) {
-    soundImg.src = "img/Museum/soundOf.png";
+    soundImg.src = "assets/img/Video/player-btn/soundOf.png";
     progressSound.value = "0";
   } else {
-    soundImg.src = "img/Museum/sound.png";
+    soundImg.src = "assets/img/Video/player-btn/sound.png";
     progressSound.value = "0.6";
   }
 }
@@ -48,16 +48,16 @@ function videoPracentege() {
 function screen() {
   console.log(`video height is ${video.style}`);
   if (window.getComputedStyle(video).height == "630px") {
-    screenImg.src = "img/Museum/screenSmall.png";
+    screenImg.src = "assets/img/Video/player-btn/screenSmall.png";
     video.style.height = "950px";
   } else {
-    screenImg.src = "img/Museum/screenSmall.png";
+    screenImg.src = "assets/img/Video/player-btn/screenSmall.png";
     video.style.height = "630px";
   }
 }
 
 play.addEventListener("click", videoStatus);
-bigPlay.addEventListener("click", videoStatus);
+//bigPlay.addEventListener("click", videoStatus);
 video.addEventListener("click", videoStatus);
 soundOf.addEventListener("click", soundOnOf);
 progressVideo.addEventListener("change", changeRange);
@@ -67,4 +67,7 @@ fullScreen.addEventListener("click", screen);
 
 skipBtns.forEach((button) => button.addEventListener("click", skip));
 
+console.log("video");
+
 //small player
+
