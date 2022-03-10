e2574212417d86ac3a464408cf078c16954966a7
Yuliya Mironova <rabkevich15@gmail.com>
2022-03-11 00:18:47 +0100

    fix: on site

--- code-example.js
+++ code-example.js
@@ -1,13 +1,11 @@
-function stringify(list) {
-  result = "";
-  result = JSON.stringify(new Node(list));
-  let answer = result.replace(/[^+\d]/g, " ");
-  answer = answer.replace(/\s{2,}/g, " ");
-  answer = answer.substring(1);
-  let answerOk = answer.replace(/\s{1,}/g, " -> ");
-  answerOk = answerOk + "null";
-  console.log(answerOk);
-  return answerOk;
-}
-new Node(1, new Node(2, new Node(3)));
-console.log(`${result}`);
+const arrUnicSort = (arr) => {
+	const thisHowMany = arr.reduce((acum, cur) => {
+		acum[cur] = (acum[cur] || 0) + 1;
+		return acum;
+	}, {});
+	const keys = Object.keys(thisHowMany);
+
+	return keys.sort((a, b) => {
+		return thisHowMany[b] - thisHowMany[a];
+	});
+};
